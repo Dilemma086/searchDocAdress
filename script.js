4aa1e3256be44c682b7d40db2ddd28e2a0f0d682
Dilemma086 <Dilemma086@yandex.ru>
2023-08-03 15:01:25 +0300

    Настроен поиск и вывод блока с врачем

--- script.js
+++ script.js
@@ -3,19 +3,32 @@ const inpSubmit = document.querySelector('.inp_submit')
 const trs = document.querySelectorAll('.card')
 const blockPlaceholder = document.querySelector('.blockPlaceholder')
 const adress = document.querySelectorAll('.adressDocTer')
+const body = document.querySelector('body')
+
 
 const searchDoc = (text) => {
+    const arr = []
+    if(text.length>1){
+        adress.forEach((item) =>{
+            const req = new RegExp(text.toLowerCase())
+            if(req.test(item.textContent.toLowerCase()) === true){
+                item.classList.add('active')
+                let li = document.createElement('li')
+                li.appendChild(document.createTextNode(item.textContent))
+                blockPlaceholder.append(li)
+            }
+        })
+    }
+
+    let oneli = document.querySelectorAll('li')
+        oneli.forEach(w => {
+            w.addEventListener('click', (e) =>{
+                inpSearch.value = e.target.textContent
+                CleaningLi()  
+        })
+    })
     // const input = e.target;
     // const req = new RegExp(input.value)
-    const arr = []
-    adress.forEach((item) =>{
-        const req = new RegExp(text)
-        
-        if(req.test(item.textContent) === true){
-            item.classList.add('active')
-            blockPlaceholder.insertAdjacentHTML('beforeend', `<li>${item.textContent}</li>`)
-        }
-    })
     // trs.forEach((item) => { // item блок одного участка с врачем и адресами
     //     const content = item.querySelector('.content').textContent // выбераем блок с адресами
     //     if(req.test(content) === true){// test возвращает true или false
@@ -33,9 +46,36 @@ const searchDoc = (text) => {
 //         console.log(dataText.match(req))
 //     })
 // }
-inpSearch.addEventListener('input', (e) => {
+// inpSearch.addEventListener('input', searchDoc)
+
+inpSubmit.addEventListener('click', () =>{
+    finalSearch()
+})
+
+
+const CleaningLi = () => {
     adress.forEach(item => item.classList.remove('active'))
+    let allLi = Array.from(document.querySelectorAll('li'))
+    allLi.map(item=>item.remove())
+}
+
+inpSearch.addEventListener('input', (e) => {
+    CleaningLi()
     searchDoc(inpSearch.value)
 })
 
-// inpSearch.addEventListener('input', searchDoc)
+
+
+const finalSearch = () => {
+       adress.forEach((item) =>{
+        if(inpSearch.value === item.textContent){
+
+            const block = item.parentNode.parentElement.childNodes[1].cloneNode(true)
+            const div = document.createElement('div')
+            div.classList.add('modalOpenTer')
+            body.append(div)
+            document.querySelector('.modalOpenTer').append(block)
+            
+        }
+    })
+}
